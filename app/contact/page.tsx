1082757ca46ab82dca3811647beda4913557eb83
Jose Solorio Ramirez <jsolorioramirez@ucdavis.edu>
2024-05-01 17:25:11 -0700

    Created Contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,13 @@
+import Card from "@/components/Card"
+import PageContainer from "@/components/PageContainer"
+
+const Contact = () => (
+  <PageContainer>
+    <Card>
+      <h2>Contact</h2>
+      <p>Feel free to reach out to me at <a href="mailto:joselsoloriojr@gmail.com" /></p>
+    </Card>
+  </PageContainer>
+)
+
+export default Contact
